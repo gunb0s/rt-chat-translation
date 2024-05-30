388634717fecd7a59c3651b96880a99e84ddbce6
cain <gunbos1031@gmail.com>
2024-05-30 10:45:24 +0900

    implement websocket, error caused 1002 Protocol Error: A WebSocket frame was sent with an unrecognized opCode of [7]

---
+++ src/main/java/com/gunb0s/rt_chat_translation/Utils.java
@@ -0,0 +1,19 @@
+package com.gunb0s.rt_chat_translation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gunb0s.rt_chat_translation.socket.Message;
+
+public class Utils {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private Utils() {
+    }
+
+    public static Message getObject(final String message) throws Exception {
+        return objectMapper.readValue(message, Message.class);
+    }
+
+    public static String getString(final Message message) throws Exception {
+        return objectMapper.writeValueAsString(message);
+    }
+}
--- src/test/java/com/gunb0s/rt_chat_translation/TranslationServiceTest.java
+++ src/test/java/com/gunb0s/rt_chat_translation/TranslationServiceTest.java
@@ -1,8 +1,11 @@
 package com.gunb0s.rt_chat_translation;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TranslationServiceTest {
@@ -10,10 +13,12 @@ class TranslationServiceTest {
     private TranslationService translationService;
 
     @Test
+    @Disabled
     void translateText() {
         String text = "Hello";
         String targetLanguage = "ko";
         String translatedText = translationService.translateText(text, targetLanguage);
-        System.out.println(translatedText);
+
+        assertThat(translatedText).isEqualTo("안녕하세요");
     }
 }

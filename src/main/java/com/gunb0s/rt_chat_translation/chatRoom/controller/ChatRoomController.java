5bf7ff95273706373f5c4a9f082cb602ae701f3a
cain <gunbos1031@gmail.com>
2024-06-02 01:45:55 +0900

    feat: create chat room

--- src/main/java/com/gunb0s/rt_chat_translation/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/gunb0s/rt_chat_translation/chatRoom/controller/ChatRoomController.java
@@ -1,10 +1,13 @@
 package com.gunb0s.rt_chat_translation.chatRoom.controller;
 
+import com.gunb0s.rt_chat_translation.auth.service.MyUserDetail;
+import com.gunb0s.rt_chat_translation.chatRoom.entity.ChatRoom;
 import com.gunb0s.rt_chat_translation.chatRoom.service.ChatRoomService;
 import com.gunb0s.rt_chat_translation.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,27 +20,37 @@ public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
     @PostMapping
-    public ResponseEntity<?> createChatRoom() {
-        chatRoomService.createChatRoom();
+    public ResponseEntity<?> createChatRoom(Authentication authentication) {
+        MyUserDetail myUserDetail = (MyUserDetail) authentication.getPrincipal();
+        ChatRoom chatRoom = chatRoomService.createChatRoom(myUserDetail.getId());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        ResponseDto.builder()
+                                .status(HttpStatus.CREATED.value())
+                                .data(chatRoom.getId())
+                                .build()
+                );
     }
 
     @GetMapping("/my")
     public ResponseEntity<?> getMyChatRooms() {
-        return ResponseEntity.ok(
-                ResponseDto.builder()
-                        .status(HttpStatus.OK.value())
-                        .build()
-        );
+        return ResponseEntity
+                .ok(
+                        ResponseDto.builder()
+                            .status(HttpStatus.OK.value())
+                            .build()
+                );
     }
 
     @GetMapping("/recent")
     public ResponseEntity<?> getRecentChatRooms() {
-        return ResponseEntity.ok(
-                ResponseDto.builder()
-                        .status(HttpStatus.OK.value())
-                        .build()
-        );
+        return ResponseEntity
+                .ok(
+                        ResponseDto.builder()
+                            .status(HttpStatus.OK.value())
+                            .build()
+                );
     }
 }
--- src/main/java/com/gunb0s/rt_chat_translation/chatRoom/entity/ChatRoom.java
+++ src/main/java/com/gunb0s/rt_chat_translation/chatRoom/entity/ChatRoom.java
@@ -3,18 +3,33 @@ package com.gunb0s.rt_chat_translation.chatRoom.entity;
 import com.gunb0s.rt_chat_translation.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "chat_rooms")
 @NoArgsConstructor
+@Getter
 public class ChatRoom extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    @Column(nullable = true)
+    private String name;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private final List<ChatRoomUser> chatRoomUsers = new ArrayList<>();
+
     @Builder
-    public ChatRoom(String id) {
-        this.id = id;
+    public ChatRoom(String name) {
+        this.name = name;
+    }
+
+    public void addUser(ChatRoomUser chatRoomUser) {
+        chatRoomUsers.add(chatRoomUser);
     }
 }
